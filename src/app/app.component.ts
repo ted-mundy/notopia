8205c331b6adef00a98bd9dbcc0f116d2d538844
Ted Mundy <ted@mundy.io>
2025-07-04 13:51:23 +0100

    feat(app): add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,15 +10,4 @@ import { invoke } from "@tauri-apps/api/tauri";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  greetingMessage = "";
-
-  greet(event: SubmitEvent, name: string): void {
-    event.preventDefault();
-
-    // Learn more about Tauri commands at https://v1.tauri.app/v1/guides/features/command
-    invoke<string>("greet", { name }).then((text) => {
-      this.greetingMessage = text;
-    });
-  }
-}
+export class AppComponent {}
